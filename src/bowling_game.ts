a4b5b4912ef988ed9f46a7f65588fb8fc22b26db
Smitha <smitha.kotian@gmail.com>
2023-11-05 21:20:07 +0000

    feat: add logic for strike, spare and frame score calculation

--- src/bowling_game.ts
+++ src/bowling_game.ts
@@ -1,6 +1,57 @@
-// function calculateTotalScore (input : String) return number
+export const calculateTotalScore = (inputFrames: string[]): number => {
+  let totalScore = 0;
+  return totalScore;
+};
 
 export const generateArrayOfFrames = (input: string): string[] => {
   //arrOfFrames
   return input.split(" ");
+};
+
+export const getFrameScore = (frameDetails: string): number => {
+  let score = 0;
+
+  if (frameDetails.charAt(0) !== "-") score = Number(frameDetails.charAt(0));
+  if (frameDetails.length === 2 && frameDetails.charAt(1) !== "-")
+    score = score + Number(frameDetails.charAt(1));
+  return score;
+};
+
+export const isFrameStrike = (frameDetails: string): boolean => {
+  return frameDetails === "X";
+};
+
+export const isFrameSpare = (frameDetails: string): boolean => {
+  return frameDetails.charAt(1) === "/";
+};
+
+export const getScoreFromNextTwoRolls = (
+  index: number,
+  framesArray: string[]
+): number => {
+  let bonusScore = 0;
+  let rollNumber1 = framesArray[index + 1].charAt(0);
+  let rollNumber2 = framesArray[index + 1].charAt(1)
+    ? framesArray[index + 1].charAt(1)
+    : framesArray[index + 2].charAt(0);
+  if (rollNumber1 === "X") bonusScore = 10;
+  else bonusScore = Number(rollNumber1);
+
+  if (rollNumber2 === "X") return bonusScore + 10;
+  else if (rollNumber2 === "-") return bonusScore;
+  else if (rollNumber2 === "/") return 10;
+  else return bonusScore + Number(rollNumber2);
+};
+
+export const getScoreFromNextRoll = (
+  index: number,
+  framesArray: string[]
+): number => {
+  let nextRoll =
+    framesArray[index].length === 3
+      ? framesArray[index].charAt(2)
+      : framesArray[index + 1].charAt(0);
+  if (nextRoll === "X") return 10;
+  else if (nextRoll === "-") return 0;
+  else return Number(nextRoll);
 };
--- tests/bowling_game.test.ts
+++ tests/bowling_game.test.ts
@@ -2,7 +2,14 @@
 //9- 9- 9- 9- 9- 9- 9- 9- 9- 9- 90
 //5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5 150
 
-import { generateArrayOfFrames } from "../src/bowling_game";
+import {
+  generateArrayOfFrames,
+  getFrameScore,
+  getScoreFromNextRoll,
+  getScoreFromNextTwoRolls,
+  isFrameSpare,
+  isFrameStrike,
+} from "../src/bowling_game";
 
 //1- 1- 1- 1- 1- 1- 1- 1- 1- 1- 1-   10
 
@@ -51,5 +58,182 @@ describe("test the function generateFrameFromInputFrames which returns an array 
       "5/",
       "5/5",
     ]);
+  });
+});
+
+describe("test function isFrameStrike to check if input is X", () => {
+  it("for input frame 'X' return true", () => {
+    expect(isFrameStrike("X")).toBe(true);
+  });
+  it("for input frame '9-' return false", () => {
+    expect(isFrameStrike("9-")).toBe(false);
+  });
+  it("for input frame '5/' return false", () => {
+    expect(isFrameStrike("5/")).toBe(false);
+  });
+});
+describe("test function isFrameSpare to check if input frame is spare", () => {
+  it("for input frame 'X' return false", () => {
+    expect(isFrameSpare("X")).toBe(false);
+  });
+  it("for input frame '9-' return false", () => {
+    expect(isFrameSpare("9-")).toBe(false);
+  });
+  it("for input frame '5/' return true", () => {
+    expect(isFrameSpare("5/")).toBe(true);
+  });
+});
+const sampleFrames1 = [
+  "X",
+  "X",
+  "X",
+  "X",
+  "X",
+  "X",
+  "X",
+  "X",
+  "X",
+  "X",
+  "X",
+  "X",
+];
+
+const sampleFrames2 = [
+  "5/",
+  "5/",
+  "5/",
+  "5/",
+  "5/",
+  "5/",
+  "X",
+  "5/",
+  "5/",
+  "5/5",
+];
+
+const sampleFrames3 = [
+  "5/",
+  "5/",
+  "5/",
+  "5/",
+  "5/",
+  "X",
+  "5-",
+  "5/",
+  "5/",
+  "5/5",
+];
+
+const sampleFrames4 = [
+  "5/",
+  "5/",
+  "5/",
+  "5/",
+  "5/",
+  "X",
+  "53",
+  "5/",
+  "5/",
+  "5/5",
+];
+describe("test function getScoreFromNextTwoRolls", () => {
+  it("for input with next 2 rolls X", () => {
+    expect(getScoreFromNextTwoRolls(6, sampleFrames1)).toBe(20);
+  });
+  it("for input with next 2 rolls X", () => {
+    expect(getScoreFromNextTwoRolls(6, sampleFrames2)).toBe(10);
+  });
+  it("for input with next 2 rolls X", () => {
+    expect(getScoreFromNextTwoRolls(5, sampleFrames3)).toBe(5);
+  });
+  it("for input with next 2 rolls X", () => {
+    expect(getScoreFromNextTwoRolls(5, sampleFrames4)).toBe(8);
+  });
+});
+
+const sampleFrames5 = [
+  "X",
+  "X",
+  "X",
+  "X",
+  "X",
+  "X",
+  "2/",
+  "X",
+  "X",
+  "X",
+  "X",
+  "X",
+];
+
+const sampleFrames6 = [
+  "5/",
+  "5/",
+  "5/",
+  "5/",
+  "5/",
+  "5/",
+  "13",
+  "5/",
+  "5/",
+  "5/5",
+];
+
+const sampleFrames7 = [
+  "5/",
+  "5/",
+  "5/",
+  "5/",
+  "5/",
+  "-3",
+  "5-",
+  "5/",
+  "5/",
+  "5/5",
+];
+
+const sampleFrames8 = [
+  "5/",
+  "5/",
+  "5/",
+  "5/",
+  "5/",
+  "X",
+  "53",
+  "5/",
+  "5/",
+  "5/5",
+];
+describe("test function getScoreFromNextRoll", () => {
+  it("for input with next roll X after spare return 10", () => {
+    expect(getScoreFromNextRoll(6, sampleFrames5)).toBe(10);
+  });
+  it("for input with next  roll 1 after the spare return 1", () => {
+    expect(getScoreFromNextRoll(5, sampleFrames6)).toBe(1);
+  });
+  it("for input with next roll - after spare return 0", () => {
+    expect(getScoreFromNextRoll(4, sampleFrames7)).toBe(0);
+  });
+  it("for input with next roll 5 after the spare in the last frame", () => {
+    expect(getScoreFromNextRoll(9, sampleFrames8)).toBe(5);
+  });
+});
+
+describe("test function getFrameScore", () => {
+  it("for frame  with just numbers returns the correct score", () => {
+    expect(getFrameScore("12")).toBe(3);
+    expect(getFrameScore("42")).toBe(6);
+    expect(getFrameScore("54")).toBe(9);
+  });
+  it("for frame starting with '-' returns the correct score", () => {
+    expect(getFrameScore("-2")).toBe(2);
+    expect(getFrameScore("-9")).toBe(9);
+    expect(getFrameScore("-4")).toBe(4);
+  });
+
+  it("for frame ending with '-' returns the correct score", () => {
+    expect(getFrameScore("2-")).toBe(2);
+    expect(getFrameScore("9-")).toBe(9);
+    expect(getFrameScore("4-")).toBe(4);
   });
 });
